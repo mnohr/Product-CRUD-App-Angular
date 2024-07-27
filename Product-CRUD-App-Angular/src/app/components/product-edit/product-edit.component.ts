ed20c32cb2756223a43c06acfe0a618a758c95ee
Manohar shrestha <nov29sthamanohar@gmail.com>
2024-07-27 15:30:03 +0545

    feat: add form using reactive form

--- Product-CRUD-App-Angular/src/app/components/product-edit/product-edit.component.ts
+++ Product-CRUD-App-Angular/src/app/components/product-edit/product-edit.component.ts
@@ -1,12 +1,43 @@
 import { Component } from '@angular/core';
+import { FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { Product } from '../../models/product.model';
+import { Router } from '@angular/router';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-product-edit',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule],
   templateUrl: './product-edit.component.html',
   styleUrl: './product-edit.component.css'
 })
 export class ProductEditComponent {
+  productForm!: FormGroup;
+  isEditing: boolean = false;
 
+  constructor( private fb: FormBuilder, private router: Router){
+
+  }
+
+  ngOnInit(): void {
+    this.productForm = this.fb.group({
+      id: [null],
+      name: ['', Validators.required],
+      description: ['', Validators.required],
+      price: [0, [Validators.required, Validators.min(0)]]
+    });
+  }
+
+  onSubmit(): void {
+    if (this.productForm.valid) {
+      const product: Product = this.productForm.value;
+      console.log(product);
+    }
+  }
 }
