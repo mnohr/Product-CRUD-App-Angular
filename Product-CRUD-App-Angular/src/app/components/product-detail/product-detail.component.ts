1e3efcd3e7f6363e4fe516996e6ccc5675d99a8c
Manohar shrestha <nov29sthamanohar@gmail.com>
2024-07-27 18:00:35 +0545

    feat: add product detail

--- Product-CRUD-App-Angular/src/app/components/product-detail/product-detail.component.ts
+++ Product-CRUD-App-Angular/src/app/components/product-detail/product-detail.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
-
+import { Component} from '@angular/core';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MatCardModule} from '@angular/material/card';
+import {MatChipsModule} from '@angular/material/chips';
+import { Product } from '../../models/product.model';
+import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-product-detail',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatChipsModule, MatProgressBarModule],
   templateUrl: './product-detail.component.html',
   styleUrl: './product-detail.component.css'
 })
 export class ProductDetailComponent {
+  longText = `The Chihuahua is a Mexican breed of toy dog. It is named for the
+  Mexican state of Chihuahua and is among the smallest of all dog breeds. It is
+  usually kept as a companion animal or for showing.`;
+  productId!: number;
+  product : Product = 
+    {
+      id: 1,
+      name: 'harry',
+      description: 'nice',
+      price: 23423
+    }
+
+  constructor(private route: ActivatedRoute){
+
+  } 
+  ngOnInit(): void {
+    this.productId =  +this.route.snapshot.paramMap.get('id')!;
+    // TODO: get data from api for product id
+    
+  }
 
 }
