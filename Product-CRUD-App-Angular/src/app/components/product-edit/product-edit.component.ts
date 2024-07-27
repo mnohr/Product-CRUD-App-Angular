3092ddc82fef2212c2ac9a30bc1b2111d1e0f252
Manohar shrestha <nov29sthamanohar@gmail.com>
2024-07-27 17:57:54 +0545

    feat: update edit form product

--- Product-CRUD-App-Angular/src/app/components/product-edit/product-edit.component.ts
+++ Product-CRUD-App-Angular/src/app/components/product-edit/product-edit.component.ts
@@ -3,7 +3,7 @@ import { FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { FormBuilder } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { Product } from '../../models/product.model';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 
@@ -20,24 +20,55 @@ import { MatButtonModule } from '@angular/material/button';
 export class ProductEditComponent {
   productForm!: FormGroup;
   isEditing: boolean = false;
+  productId!: number;
+  product: Product =
+    {
+      id: 1,
+      name: 'harry',
+      description: 'nice',
+      price: 23423
+    }
 
-  constructor( private fb: FormBuilder, private router: Router){
+  constructor(private fb: FormBuilder, private router: Router, private route: ActivatedRoute,) {
 
   }
 
   ngOnInit(): void {
+    this.productId = +this.route.snapshot.paramMap.get('id')!;
+    this.isEditing = !!this.productId;
     this.productForm = this.fb.group({
       id: [null],
       name: ['', Validators.required],
       description: ['', Validators.required],
       price: [0, [Validators.required, Validators.min(0)]]
     });
+
+    if (this.isEditing) {
+      this.loadProduct();
+    }
+  }
+
+  loadProduct(): void {
+    // todo: get data from api 
+    this.productForm.patchValue(this.product);
   }
 
   onSubmit(): void {
-    if (this.productForm.valid) {
-      const product: Product = this.productForm.value;
-      console.log(product);
+    if (this.productForm.invalid) {
+      return;
+    }
+    const product: Product = this.productForm.value;
+    if (this.isEditing) {
+      this.router.navigate(['/products']);
+    } else {
+      this.router.navigate(['/products']);
     }
   }
+
+  // onSubmit(): void {
+  //   if (this.productForm.valid) {
+  //     const product: Product = this.productForm.value;
+  //     console.log(product);
+  //   }
+  // }
 }
