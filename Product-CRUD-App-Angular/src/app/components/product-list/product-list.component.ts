eabd16a2df4807337e70692eb1ac0f9228ad8671
Manohar shrestha <nov29sthamanohar@gmail.com>
2024-07-28 17:52:58 +0545

    feat: update with api methods call and search feature

--- Product-CRUD-App-Angular/src/app/components/product-list/product-list.component.ts
+++ Product-CRUD-App-Angular/src/app/components/product-list/product-list.component.ts
@@ -1,30 +1,81 @@
-import { Component } from '@angular/core';
-import {MatTableModule} from '@angular/material/table';
+import { Component, inject } from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
 import { Product } from '../../models/product.model';
 import { Router } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { ProductService } from '../../Services/product.service';
+import { ProductSearchComponent } from '../product-search/product-search.component';
+import { MatButtonModule } from '@angular/material/button';
+import { CurrencyFormatPipe } from '../../pipes/currency-format.pipe';
 
-const ELEMENT_DATA: Product[] = [
-  {id: 1, name: 'Hydrogen', description: "3242dsdf", price: 23},
-  {id: 2, name: 'Helium', description: "fgdfg32", price: 34},
-  {id: 3, name: 'Lithium', description: "324", price: 90},
-  {id: 4, name: 'Beryllium', description: "9.0122", price: 234}
-];
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [MatTableModule],
+  imports: [
+    MatTableModule,
+    MatIconModule,
+    ProductSearchComponent,
+    MatButtonModule,
+    CurrencyFormatPipe,
+  ],
   templateUrl: './product-list.component.html',
-  styleUrl: './product-list.component.css'
+  styleUrl: './product-list.component.css',
 })
+
 export class ProductListComponent {
-  displayedColumns: string[] = ['id', 'name', 'description', 'price'];
-  dataSource = ELEMENT_DATA;
+  displayedColumns: string[] = [
+    'id',
+    'name',
+    'description',
+    'price',
+    'actions',
+  ];
 
-  constructor(private router: Router){
+  products: Product[] = [];
+  filteredProducts: Product[] = [];
+  constructor(private router: Router, private productService: ProductService) {}
 
+  ngOnInit() {
+    this.productService.getProducts().subscribe((products) => {
+      this.products = products;
+      this.filteredProducts = products;
+    });
   }
 
-  createProduct(path: string){
+  createProduct(path: string) {
     this.router.navigate([path]);
+  }
+
+  viewProduct(product: Product) {
+    console.log('View product', product);
+    this.router.navigate([`/products/${product.id}`]);
+  }
+
+  editProduct(product: Product) {
+    console.log('Edit product', product);
+    this.router.navigate([`/products/${product.id}/edit`]);
+  }
+
+  deleteProduct(product: Product): void {
+    if (
+      product.id !== null &&
+      confirm('Are you sure you want to delete this product?')
+    ) {
+      this.productService.deleteProduct(product.id).subscribe(() => {
+        this.filteredProducts = this.products.filter(
+          (p) => p.id !== product.id
+        );
+      });
+    }
+  }
+
+  onSearch(searchTerm: string): void {
+    if (searchTerm) {
+      this.filteredProducts = this.products.filter((product) =>
+        product.name.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+    } else {
+      this.filteredProducts = this.products;
+    }
   }
 }
